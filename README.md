b532f55d5ae2e73704e3ad947010e5e8366d2375
rakesh-5 <rakeshkumarswain777@gmail.com>
2021-02-03 18:45:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# zxing-ngx-scanner-xvejzt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/zxing-ngx-scanner-xvejzt)
---
+++ projects/zxing-scanner-demo/src/app/app.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+//import { MatDialog } from '@angular/material/dialog';
+import { BarcodeFormat } from '@zxing/library';
+import { BehaviorSubject } from 'rxjs';
+import { FormatsDialogComponent } from './formats-dialog/formats-dialog.component';
+import { AppInfoDialogComponent } from './app-info-dialog/app-info-dialog.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+
+  formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE
+  ];
+
+  hasDevices: boolean;
+  hasPermission: boolean;
+
+  qrResultString: string;
+
+  torchEnabled = false;
+  torchAvailable$ = new BehaviorSubject<boolean>(false);
+  tryHarder = false;
+
+  constructor() {}
+
+  clearResult(): void {
+    this.qrResultString = null;
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+  }
+
+  onDeviceSelectChange(selected: string) {
+    const device = this.availableDevices.find(x => x.deviceId === selected);
+    this.currentDevice = device || null;
+  }
+
+  openFormatsDialog() {
+    const data = {
+      formatsEnabled: this.formatsEnabled
+    };
+  }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
+
+  openInfoDialog() {
+    const data = {
+      hasDevices: this.hasDevices,
+      hasPermission: this.hasPermission
+    };
+  }
+
+  onTorchCompatible(isCompatible: boolean): void {
+    this.torchAvailable$.next(isCompatible || false);
+  }
+
+  toggleTorch(): void {
+    this.torchEnabled = !this.torchEnabled;
+  }
+
+  toggleTryHarder(): void {
+    this.tryHarder = !this.tryHarder;
+  }
+}
